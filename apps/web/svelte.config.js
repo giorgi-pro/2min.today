708d0c9384bcf0d97a4367e5f981a1274a25591b
giorgi.pro <me@giorgi.pro>
2026-03-27 02:11:56 +0100

    fix CI

--- apps/web/svelte.config.js
+++ apps/web/svelte.config.js
@@ -1,6 +1,6 @@
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
-import adapter from '@sveltejs/adapter-auto'
+import adapter from '@sveltejs/adapter-vercel'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 const dirname = path.dirname(fileURLToPath(import.meta.url))
